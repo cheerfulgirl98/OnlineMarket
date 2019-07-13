f636de2b292300e55fc46414910761610fbe2b18
cheerfulgirl98 <cheeeerfulgirl@yahoo.com>
2019-07-13 11:55:32 +0430

    orders merge

---
+++ app/src/main/java/com/sepideh/onlinemarket/navigationview/NavigationActivity.java
@@ -0,0 +1,16 @@
+package com.sepideh.onlinemarket.navigationview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sepideh.onlinemarket.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
